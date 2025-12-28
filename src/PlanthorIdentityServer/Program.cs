df3ce485ed1c7648ff2e4d1f56603bb510821aec
zovippro1996 <trunglepham1202@gmail.com>
2025-12-28 12:29:00 +0700

    feat: init web host

--- src/PlanthorIdentityServer/Program.cs
+++ src/PlanthorIdentityServer/Program.cs
@@ -1,22 +1,167 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+using Npgsql.Replication;
+
+using PlanthorIdentityServer.Data;
+
 using Quartz;
 
-var builder = WebApplication.CreateBuilder(args);
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
-// OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
-// (like pruning orphaned authorizations/tokens from the database) at regular intervals.
-builder.Services.AddQuartz(options =>
+namespace PlanthorIdentityServer;
+
+public static class Program
 {
-    options.UseSimpleTypeLoader();
-    options.UseInMemoryStore();
-});
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// Register the Quartz.NET service and configure it to block shutdown until jobs are complete.
-builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            var openIddictConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            options.UseNpgsql(openIddictConnectionString);
+            options.UseOpenIddict();
+        });
+
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        // Register the Identity services.
+        builder.Services
+            .AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders()
+            .AddDefaultUI();
+
+        // OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
+        // (like pruning orphaned authorizations/tokens from the database) at regular intervals.
+        builder.Services.AddQuartz(options =>
+        {
+            options.UseSimpleTypeLoader();
+            options.UseInMemoryStore();
+        });
+
+        // Register the Quartz.NET service and configure it to block shutdown until jobs are complete.
+        builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+        builder.Services
+            .AddOpenIddict()
+            .AddCore(options =>
+            {
+                // Configure OpenIddict to use the Entity Framework Core stores and models.
+                // Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
+                options
+                    .UseEntityFrameworkCore()
+                    .UseDbContext<ApplicationDbContext>();
+
+                // Enable Quartz.NET integration.
+                options.UseQuartz();
+            })
+            .AddClient(options =>
+            {
+                // Note: this sample uses the code flow, but you can enable the other flows if necessary.
+                options.AllowAuthorizationCodeFlow();
+
+                // Register the signing and encryption credentials used to protect
+                // sensitive data like the state tokens produced by OpenIddict.
+                options.AddDevelopmentEncryptionCertificate()
+                       .AddDevelopmentSigningCertificate();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                options
+                    .UseAspNetCore()
+                    .EnableStatusCodePagesIntegration()
+                    .EnableRedirectionEndpointPassthrough();
+
+                // Register the System.Net.Http integration and use the identity of the current
+                // assembly as a more specific user agent, which can be useful when dealing with
+                // providers that use the user agent as a way to throttle requests (e.g Reddit).
+                options.UseSystemNetHttp()
+                       .SetProductInformation(typeof(Program).Assembly);
+            })
+            .AddServer(options =>
+            {
+                // Enable the authorization, logout, token and userinfo endpoints.
+                options
+                    .SetAuthorizationEndpointUris("connect/authorize")
+                    .SetEndSessionEndpointUris("connect/logout")
+                    .SetTokenEndpointUris("connect/token")
+                    .SetUserInfoEndpointUris("connect/userinfo");
+
+                // Mark the "email", "profile" and "roles" scopes as supported scopes.
+                options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+                // Note: this sample only uses the authorization code and refresh token
+                // flows but you can enable the other flows if you need to support implicit,
+                // password or client credentials.
+                options
+                    .AllowAuthorizationCodeFlow()
+                    .AllowRefreshTokenFlow();
 
 
+                // Register the encryption credentials. This sample uses a symmetric
+                // encryption key that is shared between the server and the Api2 sample
+                // (that performs local token validation instead of using introspection).
+                //
+                // Note: in a real world application, this encryption key should be
+                // stored in a safe place (e.g in Azure KeyVault, stored as a secret).
+                options.AddEncryptionKey(new SymmetricSecurityKey(
+                    Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
 
-var app = builder.Build();
+                // Register the signing and encryption credentials.
+                options.AddDevelopmentEncryptionCertificate()
+                       .AddDevelopmentSigningCertificate();
 
-app.MapGet("/", () => "Hello World!");
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                options.UseAspNetCore()
+                       .EnableAuthorizationEndpointPassthrough()
+                       .EnableEndSessionEndpointPassthrough()
+                       .EnableTokenEndpointPassthrough()
+                       .EnableUserInfoEndpointPassthrough()
+                       .EnableStatusCodePagesIntegration();
+            })
+            .AddValidation(options =>
+            {
+                // Import the configuration from the local OpenIddict server instance.
+                options.UseLocalServer();
 
-app.Run();
+                // Register the ASP.NET Core host.
+                options.UseAspNetCore();
+            });
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseStatusCodePagesWithReExecute("~/error");
+            //app.UseExceptionHandler("~/error");
+
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.MapDefaultControllerRoute();
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
+
