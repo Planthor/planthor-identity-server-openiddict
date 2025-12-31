881eb93df6be75b7a9975bdef11543294e405761
zovippro1996 <trunglepham1202@gmail.com>
2025-12-31 15:59:23 +0700

    feat: initiate tailwindcss front end

---
+++ src/PlanthorIdentityServer/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./Areas/**/*.{cs,cshtml,html,js}",
+    "./Pages/**/*.{cs,cshtml,html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
